b56e973441a1f67a04a51218d4026419d14aaf14
Keila Pitangui <kmpitangui@gmail.com>
2026-01-07 09:27:13 -0300

    removendo lock.hcl terraform

--- aws/main.tf
+++ aws/main.tf
@@ -1,10 +1,4 @@
 
-data "aws_canonical_user_id" "current" {}
-
-output "canonical_user_id" {
-  value = data.aws_canonical_user_id.current.id
-}
-
 #bucket S3 
 resource "aws_s3_bucket" "my_bucket_teste" {
   bucket        = var.bucket
@@ -21,11 +15,15 @@ resource "aws_s3_bucket" "my_bucket_teste" {
     grant {
     type        = "CanonicalUser"
     permissions = ["FULL_CONTROL"]
-    
-    # 2. Aqui, usamos o ID que o Data Source encontrou
-    id = data.aws_canonical_user_id.current.id
   }
 
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.my_bucket_teste.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_acl" "example" {
